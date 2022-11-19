330415592f904cd331f1a9e126f358c71c369571
xi_jjun <rlawowns97@naver.com>
2022-11-20 03:25:51 +0900

    feat: add kotlin function declaration practice

---
+++ hello-kotlin/src/main/kotlin/functionpractice/FunctionPratice.kt
@@ -0,0 +1,10 @@
+fun hello(name: String) {
+    println("Hello $name!!")
+}
+
+fun hello2(name: String) = println("Hello $name!!")
+
+fun main() {
+    hello("xi-jjun")
+    hello2("kim jae jun")
+}
