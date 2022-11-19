bc36f79128b25bed5470daf4dc11fe42342d5378
xi_jjun <rlawowns97@naver.com>
2022-11-19 16:43:50 +0900

    kotlin spring practice project init

---
+++ kotlin-spring/src/main/kotlin/me/practice/kotlinspring/KotlinSpringApplication.kt
@@ -0,0 +1,11 @@
+package me.practice.kotlinspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringApplication>(*args)
+}
---
+++ kotlin-spring/src/test/kotlin/me/practice/kotlinspring/KotlinSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.practice.kotlinspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
