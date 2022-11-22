961279389a34f0bda6a04e66a788623bf761e332
xi_jjun <rlawowns97@naver.com>
2022-11-22 16:37:24 +0900

    chore: rest docs, mockmvc 의존성 추가

--- kotlin-spring/build.gradle.kts
+++ kotlin-spring/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.7.5"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    id("org.asciidoctor.jvm.convert") version "3.1.0" // rest docs
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
 }
@@ -15,12 +16,34 @@ repositories {
     mavenCentral()
 }
 
+val asciidoctorExt by configurations.creating // rest docs
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(module = "junit")
+        exclude(module = "mockito-core")
+    }
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("com.ninja-squad:springmockk:3.0.1")
+
+    // rest docs
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    asciidoctorExt("org.springframework.restdocs:spring-restdocs-asciidoctor")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+//    asciidoctor("org.springframework.restdocs:spring-restdocs-asciidoctor")
+}
+
+// rest docs
+val snippetsDir by extra {
+    file("build/generated-snippets")
 }
 
 tasks.withType<KotlinCompile> {
@@ -29,6 +52,25 @@ tasks.withType<KotlinCompile> {
         jvmTarget = "1.8"
     }
 }
+
+// rest docs
+tasks {
+    asciidoctor {
+        dependsOn(test)
+        configurations("asciidoctorExt")
+        baseDirFollowsSourceFile()  // 3
+        inputs.dir(snippetsDir)
+    }
+    register<Copy>("copyDocument") {  // 4
+        dependsOn(asciidoctor)
+        from(file("build/docs/asciidoc/index.html"))
+        into(file("src/main/resources/static/docs"))
+    }
+    bootJar {
+        dependsOn("copyDocument")  // 5
+    }
+}
+
 
 tasks.withType<Test> {
     useJUnitPlatform()
